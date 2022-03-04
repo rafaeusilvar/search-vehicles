0e474de23b7cbefe0fff6cdaa18238ac00528bda
Rafael <rafaelsilvar74@gmail.com>
2022-03-04 20:03:05 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+   const modelos = [
+    {id: 1, nome: 'Chevrolet', modelo:'Onix', motor:'1.3',},
+    {id: 2, nome: 'Chevrolet', modelo:'Corsa', motor: '1.0'},
+  ];
+
+  const nome = modelos.map(modelo => modelo.modelo)
+  console.log(nome)
+
